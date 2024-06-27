ba7010062a7299f0200e50f3194c55f355640214
Ákos Klobetz <klobetz@gmail.com>
2024-06-27 17:12:02 +0200

    Add project files.

---
+++ BlazorApp/Data/MyDBContext.cs
@@ -0,0 +1,11 @@
+﻿using BlazorApp.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp.Data
+{
+    public class MyDBContext:DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options):base(options) { }
+        public DbSet<User> users { get; set; }
+    }
+}
---
+++ BlazorApp/Model/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp.Model
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage ="A felhasználó név kitöltése kötelező!")]
+        [MaxLength(100)]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "A jelszó mező kitöltése kötelező!")]
+        [MaxLength(128)]
+        public string Password { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Role { get; set; }
+    }
+}
